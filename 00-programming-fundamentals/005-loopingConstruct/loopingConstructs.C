274ec81242950fb1526b9f76ba25820266710646
Erbynn <john.erbynn@gmail.com>
2019-10-22 20:10:52 +0100

    earliest date determiner

--- 00-programming-fundamentals/005-loopingConstruct/loopingConstructs.C
+++ 00-programming-fundamentals/005-loopingConstruct/loopingConstructs.C
@@ -14,19 +14,19 @@ int main(){
 	// excercise 1
 
 	Date d1{};
-	d1.year = 1233;
-	d1.month = 10;
-	d1.day = 3;
+	d1.year = 2019;
+	d1.month = 5;
+	d1.day = 6;
 
 	Date d2{};
 	d2.year = 2010;
 	d2.month = 5;
-	d2.day = 4;
+	d2.day = 6;
 
 	Date d3{};
-	d3.year = 1944;
+	d3.year = 2019;
 	d3.month = 5;
-	d3.day = 6;
+	d3.day = 10;
 
 	vector<Date> myVec{d1, d2, d3};
 
@@ -84,8 +84,50 @@ int main(){
 
 	
  
-	// exercise 2
+	// exercise 2---date comparison
+		
+	if( (d1.year > d2.year) && (d1.year > d3.year)){
+		cout << "Earliest: " << d1.day << "-" << d1.month << "-" << d1.year << endl;
+	}
+	else if( (d2.year > d1.year) && (d2.year > d3.year)){	
+		cout << "Earliest: " << d2.day << "-" << d2.month << "-" << d2.year << endl;
+	}
+	else if( (d3.year > d1.year) && (d3.year > d2.year) ){
+		cout << "Earliest: " << d3.day << "-" << d3.month << "-" << d3.year << endl;
+	}
+	else{
+		
+		if( (d1.month > d2.month) && (d1.month > d3.month)){
+			cout << "Earliest: " << d1.day << "-" << d1.month << "-" << d1.year << endl;
+		}
+		
+		else if( (d2.month > d1.month) && (d2.month > d3.month)){
+			cout << "Earliest: " << d2.day << "-" << d2.month << "-" << d2.year << endl;
+		}
+		
+		else if( (d3.month > d1.month) && (d3.month > d2.month)){
+			cout << "Earliest: " << d3.day << "-" << d3.month << "-" << d3.year << endl;
+		}
+		else {
+			
+			if( (d1.day > d2.day) && (d1.day > d3.day)){
+				cout << "Earliest: " << d1.day << "-" << d1.month << "-" << d1.year << endl;
+			}
 
-	
+			else if( (d2.day > d1.day) && (d2.day > d3.day)){
+				cout << "Earliest: " << d2.day << "-" << d2.month << "-" << d2.year << endl;
+			}
+
+			else if( (d3.day > d1.day) && (d3.day > d2.day)){
+				cout << "Earliest: " <<  d3.day << "-" << d3.month << "-" << d3.year << endl;
+			}
+			else {
+				cout << "Same Dates: " ;
+				cout << d1.day << "-" << d1.month << "-" << d1.year << " = ";
+				cout << d2.day << "-" << d2.month << "-" << d2.year << " = ";
+				cout << d3.day << "-" << d3.month << "-" << d3.year << endl;
+			}
+		}
+	}
 
 }
